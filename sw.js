a4cd1e94d28caed63eb7c4c3ffd38eca6e153344
verticalgrain <jamie@verticalgraindesign.com>
2021-02-22 10:36:01 -0800

    Updates manifest and service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 
 AMP_SW.init({
+    assetCachingOptions: [{
+        regexp: /\.(png|jpg|mp3)/,
+        cachingStrategy: 'CACHE_FIRST'
+    }]
 });
